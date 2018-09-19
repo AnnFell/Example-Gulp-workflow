b600569bc12a8ea85aae9f9864964848e8a8696f
AnnFell <annelottef@gmail.com>
2018-09-19 16:25:48 +0200

    added browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 const gulp = require('gulp'),
+      browserSync = require('browser-sync').create(),
       eslint = require('gulp-eslint');
 
 
 gulp.task('watch', function(){
+
+  browserSync.init({
+    server: "./"
+  });
+
   gulp.watch('app/js/**/*.js', ['linter']);
+  gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 gulp.task('linter', function(){
